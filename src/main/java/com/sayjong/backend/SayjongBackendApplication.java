54b5b555a88752b74cacd59292a51d2ed82e50f5
Jeongyeun <ruthie0219@naver.com>
2025-10-05 16:35:46 +0900

    Initial commit

---
+++ src/main/java/com/sayjong/backend/SayjongBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sayjong.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayjongBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayjongBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sayjong/backend/SayjongBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayjong.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SayjongBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
