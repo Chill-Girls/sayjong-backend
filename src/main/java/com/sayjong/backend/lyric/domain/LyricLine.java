693a0b87d615f0643ea31b5c9fe67520e8fc894c
Jeongyeun <ruthie0219@naver.com>
2025-10-08 01:53:46 +0900

    nativAudio->nativAudioUrl변수명 수정

--- src/main/java/com/sayjong/backend/lyric/domain/LyricLine.java
+++ src/main/java/com/sayjong/backend/lyric/domain/LyricLine.java
@@ -36,7 +36,7 @@ public class LyricLine {
 
     @Lob
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String nativeAudio; //원어민(한국어발음)오디오
+    private String nativeAudioUrl; //원어민(한국어발음)오디오 URL
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "song_id", nullable = false)
--- src/main/java/com/sayjong/backend/lyric/domain/LyricSyllable.java
+++ src/main/java/com/sayjong/backend/lyric/domain/LyricSyllable.java
@@ -35,7 +35,7 @@ public class LyricSyllable {
 
     @Lob
     @Column(nullable = false, columnDefinition = "TEXT")
-    private String nativeAudio; //원어민(한국어발음)오디오
+    private String nativeAudioUrl; //원어민(한국어발음)오디오 URL
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "line_no", nullable = false)
