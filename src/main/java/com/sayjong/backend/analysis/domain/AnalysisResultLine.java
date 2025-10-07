ef803b127e93bc6e686c51556172a589609cd24b
Jeongyeun <ruthie0219@naver.com>
2025-10-08 02:27:38 +0900

    wrongSegment테이블 분리

--- src/main/java/com/sayjong/backend/analysis/domain/AnalysisResultLine.java
+++ src/main/java/com/sayjong/backend/analysis/domain/AnalysisResultLine.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "analysis_result_line")
 @Getter
@@ -22,8 +25,9 @@ public class AnalysisResultLine {
     @Column(nullable = false, length = 100)
     private String feedbackText; //피드백 문구
 
-    @Column(columnDefinition = "JSON")
-    private String wrongSegments; //오발음 구간 배열(JSON 문자열로 저장)
+    @OneToMany(mappedBy = "analysisResultLine", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<WrongSegment> wrongSegments = new ArrayList<>(); //오발음구간(별도의 테이블로 분리하여 관리)
 
     @Column(nullable = false)
     private Integer pronounceOk; //발음일치율
